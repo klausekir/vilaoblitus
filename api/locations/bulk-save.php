83ceb54f2e7c028df9124324e7e9780c51ace49e
Klaus Eduardo Kirner <kkirner.kpmg+SABESP@sabesp.com.br>
2025-11-27 08:49:14 -0300

    Force sync local -> remote main

--- api/locations/bulk-save.php
+++ api/locations/bulk-save.php
@@ -11,6 +11,11 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Content-Type');
 
+// Desabilitar cache
+header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+header('Pragma: no-cache');
+header('Expires: 0');
+
 require_once '../config.php';
 
 // Get database connection
